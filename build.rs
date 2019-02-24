977afe48d0a5cf254a092013fe3c23028a2a1e05
Jonas Schievink <jonasschievink@gmail.com>
2019-02-24 23:19:57 +0100

    Only provide a default .gdbinit and allow customization

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
-use std::env;
-use std::fs::File;
-use std::io::Write;
-use std::path::PathBuf;
+use std::{
+    env,
+    fs::{self, File},
+    io::Write,
+    path::PathBuf,
+};
 
 fn main() {
     // Put the linker script somewhere the linker can find it
@@ -11,6 +13,11 @@ fn main() {
         .write_all(include_bytes!("memory.x"))
         .unwrap();
     println!("cargo:rustc-link-search={}", out.display());
+
+    // Create default .gdbinit if none exists
+    if fs::metadata(".gdbinit").is_err() {
+        fs::copy(".gdbinit-openocd", ".gdbinit").unwrap();
+    }
 
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=memory.x");
