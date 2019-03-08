d89933b09a7306addbaefe3ec16cb8ac956d2f80
Ferdia McKeogh <ferdia@mckeogh.tech>
2019-03-08 19:25:21 +0100

    fix: AdStructure parsing

--- src/ble/bytes.rs
+++ src/ble/bytes.rs
@@ -64,8 +64,8 @@ impl<'a, T: ?Sized> BytesOr<'a, T> {
 impl<'a, T: FromBytes<'a>> FromBytes<'a> for BytesOr<'a, T> {
     fn from_bytes(bytes: &mut &'a [u8]) -> Result<Self, Error> {
         {
-            let mut bytes = &mut *bytes;
-            T::from_bytes(&mut bytes)?;
+            let mut bytes = &mut &**bytes;
+            T::from_bytes(bytes)?;
             if !bytes.is_empty() {
                 return Err(Error::IncompleteParse);
             }
@@ -82,7 +82,7 @@ impl<'a, T: FromBytes<'a>> FromBytes<'a> for BytesOr<'a, T> {
 impl<'a, T: FromBytes<'a>> FromBytes<'a> for BytesOr<'a, [T]> {
     fn from_bytes(bytes: &mut &'a [u8]) -> Result<Self, Error> {
         {
-            let mut bytes = &mut *bytes;
+            let mut bytes = &mut &**bytes;
             while !bytes.is_empty() {
                 T::from_bytes(&mut bytes)?;
             }
