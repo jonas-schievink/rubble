e0bca7b56aef3931df142a9deb5a28c9b8e6951e
Jonas Schievink <jonasschievink@gmail.com>
2019-10-06 17:26:35 +0200

    Enforce that rubble-nrf52 is built with feature on

---
+++ rubble-nrf52/build.rs
@@ -0,0 +1,15 @@
+use std::env;
+
+fn main() {
+    let features = &["52810", "52832", "52840"];
+    let count = features
+        .iter()
+        .filter(|name| env::var_os(format!("CARGO_FEATURE_{}", name)).is_some())
+        .count();
+
+    match count {
+        0 => panic!("no device feature enabled; please enable one of the following features, matching your target device: {:?}", features),
+        1 => {},
+        _ => panic!("{} device features enabled; please only enable one", count),
+    }
+}
