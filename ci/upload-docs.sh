09e178cec4e829b866094ff0b9b349ebb718d554
Jonas Schievink <jonasschievink@gmail.com>
2019-11-14 00:56:09 +0100

    Fix inverted upload-docs.sh logic

--- ci/upload-docs.sh
+++ ci/upload-docs.sh
@@ -2,13 +2,18 @@
 
 set -o errexit
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+echo "TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
+echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
+
+if [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
     exit
 fi
 
 if [ "$TRAVIS_BRANCH" != "master" ]; then
     exit
 fi
+
+echo "Uploading documentation to GitHub Pages..."
 
 echo "<meta http-equiv=refresh content=0;url=rubble/index.html>" > target/doc/index.html
 sudo pip install ghp-import
