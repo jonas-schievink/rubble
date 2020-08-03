6532c1010fcf3c6de3934137d0331bbb2b086c62
Jonas Schievink <jonasschievink@gmail.com>
2020-08-03 23:55:49 +0200

    wip

--- rubble/src/link/queue.rs
+++ rubble/src/link/queue.rs
@@ -22,6 +22,7 @@
 use crate::link::data::{self, Llid};
 use crate::link::{MIN_DATA_PAYLOAD_BUF, MIN_DATA_PDU_BUF};
 use crate::{bytes::*, Error};
+use core::cell::{Cell, RefCell};
 use heapless::consts::U1;
 use heapless::spsc::{self, MultiCore};
 
@@ -189,6 +190,105 @@ impl<T> Consume<T> {
             consume: result.is_ok(),
             result,
         }
+    }
+}
+
+pub struct RefCellQueue {
+    buf: RefCell<[u8; MIN_DATA_PDU_BUF]>,
+    full: Cell<bool>,
+}
+
+impl RefCellQueue {
+    pub const fn new() -> Self {
+        Self {
+            buf: RefCell::new([0; MIN_DATA_PDU_BUF]),
+            full: Cell::new(false),
+        }
+    }
+}
+
+impl<'a> PacketQueue for &'a mut RefCellQueue {
+    type Producer = RefCellProducer<'a>;
+    type Consumer = RefCellConsumer<'a>;
+
+    fn split(self) -> (Self::Producer, Self::Consumer) {
+        let p = RefCellProducer { queue: self };
+        let c = RefCellConsumer { queue: self };
+
+        (p, c)
+    }
+}
+
+pub struct RefCellProducer<'a> {
+    queue: &'a RefCellQueue,
+}
+
+impl<'a> Producer for RefCellProducer<'a> {
+    fn free_space(&self) -> u8 {
+        if self.queue.full.get() {
+            0
+        } else {
+            MIN_DATA_PDU_BUF as u8
+        }
+    }
+
+    fn produce_dyn(
+        &mut self,
+        payload_bytes: u8,
+        f: &mut dyn FnMut(&mut ByteWriter<'_>) -> Result<Llid, Error>,
+    ) -> Result<(), Error> {
+        assert!(usize::from(payload_bytes) <= MIN_DATA_PAYLOAD_BUF);
+
+        if self.queue.full.get() {
+            return Err(Error::Eof);
+        }
+
+        let mut buf = self.queue.buf.borrow_mut();
+
+        let mut writer = ByteWriter::new(&mut buf[2..]);
+        let free = writer.space_left();
+        let llid = f(&mut writer)?;
+        let used = free - writer.space_left();
+
+        let mut header = data::Header::new(llid);
+        header.set_payload_length(used as u8);
+        header.to_bytes(&mut ByteWriter::new(&mut buf[..2]))?;
+
+        self.queue.full.set(true);
+        Ok(())
+    }
+}
+
+pub struct RefCellConsumer<'a> {
+    queue: &'a RefCellQueue,
+}
+
+impl<'a> Consumer for RefCellConsumer<'a> {
+    fn has_data(&self) -> bool {
+        self.queue.full.get()
+    }
+
+    fn consume_raw_with<R>(
+        &mut self,
+        f: impl FnOnce(data::Header, &[u8]) -> Consume<R>,
+    ) -> Result<R, Error> {
+        if !self.has_data() {
+            return Err(Error::Eof);
+        }
+
+        let buf = self.queue.buf.borrow();
+
+        let mut bytes = ByteReader::new(&*buf);
+        let raw_header: [u8; 2] = bytes.read_array().unwrap();
+        let header = data::Header::parse(&raw_header);
+        let pl_len = usize::from(header.payload_length());
+        let raw_payload = bytes.read_slice(pl_len)?;
+
+        let res = f(header, raw_payload);
+        if res.consume {
+            self.queue.full.set(false);
+        }
+        res.result
     }
 }
 
@@ -437,7 +537,25 @@ pub fn run_tests(queue: impl PacketQueue) {
     // FIXME: This test could do a lot more
 }
 
-#[test]
-fn simple_queue() {
-    run_tests(&mut SimpleQueue::new());
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple_queue() {
+        run_tests(&mut SimpleQueue::new());
+    }
+
+    #[test]
+    fn refcell_queue() {
+        run_tests(&mut RefCellQueue::new());
+    }
+
+    #[test]
+    fn simple_queue_is_send() {
+        fn assert_send<T: Send>() {}
+
+        assert_send::<SimpleConsumer<'static>>();
+        assert_send::<SimpleProducer<'static>>();
+    }
 }
