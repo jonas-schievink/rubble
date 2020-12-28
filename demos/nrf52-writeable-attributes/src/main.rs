033b1563f2795e1e99c3a98293b0f5f968289764
noloerino <jhshi07@gmail.com>
2020-12-27 20:11:09 -0800

    Uncomment features for boards besides 52832

--- demos/nrf52-writeable-attributes/src/main.rs
+++ demos/nrf52-writeable-attributes/src/main.rs
@@ -3,12 +3,12 @@
 #![no_std]
 #![no_main]
 
-// #[cfg(feature = "52810")]
-// use nrf52810_hal as hal;
-// #[cfg(feature = "52832")]
+#[cfg(feature = "52810")]
+use nrf52810_hal as hal;
+#[cfg(feature = "52832")]
 use nrf52832_hal as hal;
-// #[cfg(feature = "52840")]
-// use nrf52840_hal as hal;
+#[cfg(feature = "52840")]
+use nrf52840_hal as hal;
 
 use core::cmp;
 use core::sync::atomic::{compiler_fence, Ordering};
