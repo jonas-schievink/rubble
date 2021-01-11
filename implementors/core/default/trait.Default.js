fae954d1a52276539d1caa858e879ff18bad0a75
jonas-schievink <jonas-schievink@users.noreply.github.com>
2021-01-11 19:49:07 +0000

    deploy: a2a8c94e7dbb18f9514fb59b68ce6d945ffe9e41

--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["rubble"] = [];
+implementors["rubble"] = [{"text":"impl Default for AttributeAccessPermissions","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
