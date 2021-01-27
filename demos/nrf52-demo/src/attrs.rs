d965126dc5d5ed1ae665cda3f1db25c1fa7e9659
Paul LaCrosse <paul@buckspotter.com>
2021-01-27 13:03:18 -0500

    support nRF52811 (#176)

--- demos/nrf52-demo/src/attrs.rs
+++ demos/nrf52-demo/src/attrs.rs
@@ -2,6 +2,8 @@
 
 #[cfg(feature = "52810")]
 use nrf52810_hal as hal;
+#[cfg(feature = "52811")]
+use nrf52811_hal as hal;
 #[cfg(feature = "52832")]
 use nrf52832_hal as hal;
 #[cfg(feature = "52833")]
