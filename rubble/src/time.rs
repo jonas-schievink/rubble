2cb2b597d618b89b3fb6413e0db70b62c7852c5c
Daniel Franklin <daniel@danielzfranklin.org>
2022-01-15 13:08:06 +0000

    Upgrade defmt to 0.3.0

--- rubble/src/time.rs
+++ rubble/src/time.rs
@@ -130,7 +130,7 @@ impl fmt::Debug for Duration {
 
 impl defmt::Format for Duration {
     fn format(&self, fmt: defmt::Formatter<'_>) {
-        defmt::write!(fmt, "{=u32:µs}s", self.0);
+        defmt::write!(fmt, "{=u32:us}s", self.0);
     }
 }
 
